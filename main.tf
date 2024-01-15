69371d1501af9003bc0a718c0a540b8cba8cf939
chandradevops10 <153185234+chandradevops10@users.noreply.github.com>
2024-01-15 13:59:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-062a49a8152e4c031"
+  ami                    = "ami-02a66cf05465c373f"
   instance_type          = "t2.micro"
   availability_zone      = "eu-west-1a"
   key_name               = "chandraEC2Test1"
@@ -149,7 +149,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-062a49a8152e4c031"
+  ami                    = "ami-02a66cf05465c373f"
   instance_type          = "t2.micro"
   availability_zone      = "eu-west-1b"
   key_name               = "chandraEC2Test1"
@@ -164,7 +164,7 @@ resource "aws_instance" "webserver2" {
 
 #Create EC2 Instance
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-062a49a8152e4c031"
+  ami                    = "ami-02a66cf05465c373f"
   instance_type          = "t2.micro"
   availability_zone      = "eu-west-1a"
   key_name               = "chandraEC2Test1"
@@ -176,7 +176,7 @@ resource "aws_instance" "appserver1" {
 }
 
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-062a49a8152e4c031"
+  ami                    = "ami-02a66cf05465c373f"
   instance_type          = "t2.micro"
   availability_zone      = "eu-west-1b"
   key_name               = "chandraEC2Test1"
